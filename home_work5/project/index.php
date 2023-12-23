d8dbbceaa50e23393ca0a811680112b6ba7e0e9e
Муслим <98447082+MaJloN8@users.noreply.github.com>
2023-12-23 22:28:24 +0300

    Add files via upload

---
+++ home_work5/project/index.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+		<meta name="description" content="Популярные языки программирования">
+		<meta name="keywords" content="Языки, программирование,Востребованные, Python, Java, JS, JavaScript,Golang,Kotlin">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Языки программирования</title>
+		<link rel="stylesheet" href="stelee.css">
+		<meta name="robots" content="index, follow">
+		<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
+    </head>
+	<body>
+	
+<div style="width: 58%; height:100px; position:relative; left:21%; ">
+	<div class="menu">
+
+	<div class="logo">
+	<img src="logo.png"  width="100" href="vh.html?do=logout" height="100" class="krugi" alt="logo" align="left"/>
+</div>
+	<div class="knopki" align="center" >
+	<ul id="navbar">
+<li><a href="index.php">Главная</a></li>
+   <li><a href="chasha.php">Python</a>
+
+   <li><a href="vid.php">Java, JS</a>
+
+   <li><a href="kal.php">Golang </a>
+   
+   <li><a href="tabak.php">Kotlin</a>
+
+		</ul>
+</div>
+</div>
+
+<br><br><br><br><br><br>
+
+<h1>Самые востребованные языки программирования в 2023 году</h1>
+
+<p>Выбор языка программирования — важное стратегическое решение для тех, кто строит свою карьеру в сфере ИТ. В первую очередь все зависит от направления, 
+где хочется проявить свои таланты. Но важно «поймать» баланс между амбициями и потребностями рынка труда, чтобы стать по-настоящему ценным специалистом.
+Какие языки программирования оказались самыми популярными в России в 2023 году?
+TAdviser опубликовал результаты исследования учебного центра IBS, в котором определили 5 самых актуальных языков, опираясь на мнение IT-специалистов. Разберем их подробнее!</p>
+<center>
+<img src="foto1.jpg"  width="1000" height="500"  alt="statistica"  >
+</center>
+<br><br>
+<h3>Выбор популярности языка программирования</h3>
+<br>
+<p>Популярность языка программирования исчисляется разными способами: от количества кода, написанного на нём, до количества открытых вакансий и программ по изучению. 
+У каждого, кто начинает карьеру в разработке, неизменно возникает вопрос: а что сейчас востребовано?
+ с какого языка проще всего начать, какие карьерные перспективы открываются при выборе того или иного направления?
+Мы изучили один из самых авторитетных рейтингов и рассказываем о самых актуальных языках программирования, их особенностях и сферах применения.</p>
+<br><br>
+</body>
+</html>
