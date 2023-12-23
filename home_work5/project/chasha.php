8103fa9814c668e3b6b95a320af22da41bf775a1
Муслим <98447082+MaJloN8@users.noreply.github.com>
2023-12-23 21:22:33 +0300

    Add files via upload

---
+++ home_work5/project/chasha.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+		<meta name="description" content="Python, его особенности и где используют Python">
+		<meta name="keywords" content="Python">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Python</title>
+		<link rel="stylesheet" href="stelee.css">
+		<meta name="robots" content="index, follow">
+    </head>
+	<body>
+<div style="width: 58%; height:100px; position:relative; left:21%; ">
+	<div class="menu">
+
+	<div class="logo">
+	<img src="logo.png"  width="100" href="vh.html?do=logout" height="100" class="krugi" alt="logo" align="left"/>
+</div>
+	<div class="knopki" align="center" >
+	<ul id="navbar">
+<li><a href="index.php">Главная</a></li>
+   <li><a href="chasha.php">Python</a>
+
+   <li><a href="vid.php">Java, JS</a>
+
+   <li><a href="kal.php">Golang </a>
+   
+   <li><a href="tabak.php">Kotlin</a>
+
+		</ul>
+</div>
+</div>
+<br><br><br><br><br><br>
+
+<h1>Python</h1>
+<center>
+<img src="foto33.jpg"  width="850" href="vh.html?do=logout" height="400"  alt="Python"  >
+</center>
+<p>1 место
+«Простой и лаконичный» <b>Python</b> (30%)
+Идеальный язык для начинающих с простым синтаксисом. Python держит пальму первенства в разных рейтингах с 2022 года! В чем его феномен?
+ Разработчики любят Python за простоту и универсальность – язык применяют в разработке веб-приложений, игр, программ, а также в работе с базами данных
+ . А в последнее время язык получил распространение в машинном обучении и изучении ИИ.
+<br>
+Бонус популярности языка – большое сообщество и, соответственно, широкая экосистема пакетов и библиотек для самых разных целей.
+<br><br>
+<b>Где используют:</b> веб-разработка, разработка и тестирование ПО, Data Science и машинное обучение
+<br><br>
+<b>Особенности:</b> простота и понятность делает Python языком для безболезненного входа в мир программирования. 
+Но именно как язык для работы он получил применение в автоматизации задач, анализе и визуализации данных.
+<br><br>
+<b>Python совместим с Java, C и C++ </b>– модули, написанные на разных языках, можно сочетать в одном проекте.
+ Мультиплатформенность и мультипарадигменность расширяют спектр задач, которые можно решить с его помощью. Однако, Python – интерпретируемый язык, а значит, 
+ написанные на нем программы могут работать медленнее остальных.
+</p>
+<br><br>
+</body>
+</html>
---
+++ home_work5/project/tabak.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+				<meta name="description" content="Kotlin, где применяется и какие особенности">
+				<meta name="keywords" content="Kotlin">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				
+        <title>Kotlin</title>
+		<link rel="stylesheet" href="stelee.css">
+		<meta name="robots" content="index, follow">
+    </head>
+	<body>
+<div style="width: 58%; height:100px; position:relative; left:21%; ">
+	<div class="menu">
+
+	<div class="logo">
+	<img src="logo.png"  width="100" href="vh.html?do=logout" height="100" class="krugi" alt="logo" align="left"/>
+</div>
+	<div class="knopki" align="center" >
+	<ul id="navbar">
+<li><a href="index.php">Главная</a></li>
+   <li><a href="chasha.php">Python</a>
+
+   <li><a href="vid.php">Java, JS</a>
+
+   <li><a href="kal.php">Golang </a>
+   
+   <li><a href="tabak.php">Kotlin</a>
+
+		</ul>
+</div>
+</div>
+<br><br><br><br><br><br>
+<h1>
+Kotlin
+</h1>
+<p>
+<b>Kotlin</b> – структурированный и многоцелевой язык программирования, разработанный компанией JetBrains.
+Он разрабатывался как более “компактная” версия Java и вышел легче на 40%. При этом, с Java он полностью совместим и открывается на его виртуальной машине (JVM).
+<br><br>
+<b>Где используют:</b> мобильная и серверная разработка.
+<br><br>
+<b>Возможности:</b> на Kotlin можно писать кроссплатформенный код, однако самым популярным направлением его применения остается 
+ОС Android – в 2017 году Google провозгласил его официальным языком разработки приложений под ОС на ряду с С++ и Java.
+На сегодняшний день – 80% самых популярных приложений в Play Market написаны с помощью <b>Kotlin</b>.
+</p>
+<img src="foto66.jpg"  width="700" href="vh.html?do=logout" height="500" class="krugi" alt="Kotlin " />
+
+
+<br><br>
+<body>
+</html>
